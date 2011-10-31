06f8c1e7033ac26e56e263f833f6e59ebd8330cb
maximomussini@gmail.com <maximomussini@gmail.com@da29dd1e-9bc0-f955-a5fb-3ad85e60ca74>
2011-10-31 04:53:27 +0000

    -Muestra los condados que están cerca de un punto. -Permite ingresar una dirección. Hay que arreglar los parámetros que se le pasan al servicio de geocoding.

---
+++ SIGTest/Views/WelcomePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SIGTest.Views
+{
+    public partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
