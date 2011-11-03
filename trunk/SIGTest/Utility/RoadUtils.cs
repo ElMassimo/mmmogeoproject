1c502b93ca12cf8a7b4c767ce9a164454d8c9ef6
maximomussini@gmail.com <maximomussini@gmail.com@da29dd1e-9bc0-f955-a5fb-3ad85e60ca74>
2011-11-03 18:38:31 +0000

    Falta que quede bueno

--- trunk/SIGTest/Utility/RoadUtils.cs
+++ trunk/SIGTest/Utility/RoadUtils.cs
@@ -7,7 +7,10 @@ namespace SIGTest.Utility
 {
     public static class RoadUtils
     {
+        private const int CAR_MINIMUM_SPEED = 60;
+        private const int CAR_MAXIMUM_SPEED = 140;
         private const string CAR_IMAGES_PATH = "../Assets/Images/Cars/car_{0}.png";
+        private static Random _numberGenerator = new Random();
 
         public static BitmapImage GetCarImage(object carName)
         {
@@ -19,6 +22,11 @@ namespace SIGTest.Utility
             double x = lastPoint.X - firstPoint.X;
             double y = lastPoint.Y - firstPoint.Y;
             return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        public static double GetRandomSectionSpeed()
+        {
+            return CAR_MINIMUM_SPEED + (CAR_MAXIMUM_SPEED - CAR_MINIMUM_SPEED) * _numberGenerator.NextDouble();
         }
     }
 }
